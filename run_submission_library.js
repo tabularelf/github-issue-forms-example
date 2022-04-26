8b8fa20b26b246149f36128d6296bd38ed113d54
TabularElf <grimcoder@live.co.uk>
2022-04-27 03:44:24 +1000

    Update run_submission_library.js

--- run_submission_library.js
+++ run_submission_library.js
@@ -32,17 +32,17 @@ content += "---\n";
 content += "${post}";
 
 
-if (fs.existsSync("./libs")) {
-	if (!fs.existsSync("./libs/${user}")) {
-		fs.mkdirSync("./libs/${user}");
+if (fs.existsSync("libs")) {
+	if (!fs.existsSync("libs/${user}")) {
+		fs.mkdirSync("libs/${user}");
 	}
 	
-	fs.stat("./libs/${user}/${title}.md", function(err, stat) {
+	fs.stat("libs/${user}/${title}.md", function(err, stat) {
 		if(err == null) {
 			console.log('File exists');
 		} else if(err.code === 'ENOENT') {
 			// file does not exist
-			fs.writeFileSync("./libs/${user}/${title}.md", content);
+			fs.writeFileSync("libs/${user}/${title}.md", content);
 		} else {
 			console.log('Some other error: ', err.code);
 		}
