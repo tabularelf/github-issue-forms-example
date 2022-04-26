6592c6f824cb39bbff32901065b77fe569662d6d
TabularElf <grimcoder@live.co.uk>
2022-04-27 03:12:20 +1000

    Experimenting...

---
+++ run-submission-library.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const eventPayload = require(process.env.GITHUB_EVENT_PATH);
+const library = require('./submission-library.json');
+
+const user = eventPayload.sender.login;
+const [title, description, authors.split(","), link, version, tags.split(","), post] = Object.values(library);
+
+var content = @"---
+title: ${title}
+description: ${description}
+link: ${link}
+version: ${verison}";
+
+content += "\ntags:\n";
+
+for (var entry in ${tags}) {
+	content += "  - ${tags[entry]}\n";
+}
+
+content += "\authors:\n";
+
+for (var entry in ${authors}) {
+	content += "  - ${authors[entry]}\n";
+}
+
+content += "---\n";
+
+content += "${post}";
+
+
+
+if !(fs.existsSync("//libs//${user}//${title}.md")) {
+  fs.writeFileSync("//libs//${user}//${title}.md", content);
+}
+
+
